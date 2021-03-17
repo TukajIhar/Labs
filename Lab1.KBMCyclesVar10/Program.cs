69f65c55c69659bfa45b151637ce2c1322c98ac3
TukajIhar <79803317+TukajIhar@users.noreply.github.com>
2021-03-17 17:55:19 +0300

    Add files via upload

---
+++ Lab1.KBMCyclesVar10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._2Var10
+{
+    class Program
+    {
+               static void Main(string[] args)
+        {
+            Console.WriteLine("Введите eps"); 
+            double eps = double.Parse(Console.ReadLine());
+            double s = 0; 
+            double z = 1; 
+            int n = 0;
+                       
+            while (z > eps)
+            {
+                z = 1/(Math.Pow(2*n+1,2));
+                s += z;
+                n++;
+            } 
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
