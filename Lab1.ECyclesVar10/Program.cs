d5029abbb89c20986427f27e33bd349d84087600
TukajIhar <79803317+TukajIhar@users.noreply.github.com>
2021-03-17 17:57:42 +0300

    Add files via upload

---
+++ Lab1.ECyclesVar10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Var10
+{
+    class Program
+    {
+        static double F(double x)
+        {
+            return 2*x* Math.Sin(x)-Math.Cos(x);
+        }
+               static void Main(string[] args)
+        {
+            Random random = new Random();
+            double X = random.Next(4/10);
+            double Xlast = random.Next(1);
+            double XGrandLast;
+            double eps = 0.1;
+            Console.WriteLine($"X0 = {X}");
+            double dX = double.MaxValue;
+            while (Math.Abs(dX) < eps)
+            {
+                XGrandLast = Xlast;
+                Xlast= X;
+                X -= F(Xlast) * (Xlast - XGrandLast) / (F(Xlast) - F(XGrandLast));
+                dX = X - Xlast;
+            }
+            Console.WriteLine($"X = {X}");
+            Console.WriteLine($"F(x) = {F(X)}");
+            Console.ReadKey();
+        }
+    }
+}
